c666533fe3e935758f56cda1ec8557fd159e2af6
nourhaneS <ns5210@nyu.edu>
2023-05-30 12:20:53 +0400

    Adobe Illustrator Jade Belt Bridge drawing

---
+++ Adobe Illustrator/script.js
@@ -0,0 +1,5 @@
+let insideOfBridge = document.getElementById("Layer_3")
+
+insideOfBridge.addEventListener("click", ()=> {
+	document.getElementById("textbox").innerText = "Great job, you clicked on the inside of the Jade Belt Bridge!"
+})
